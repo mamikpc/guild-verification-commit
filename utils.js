c4ebefafdedbfa750d619f1f5e50f6ebf1d9bd1d
mamikpc <90100443+mamikpc@users.noreply.github.com>
2026-04-24 17:32:15 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T14:32:00.793Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-24T14:32:14.495Z
+Commit: Fix broken links in documentation
