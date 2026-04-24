fc9b7b2be7062e91a01994d9f8036079ac811bdb
mamikpc <90100443+mamikpc@users.noreply.github.com>
2026-04-24 17:31:10 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T14:30:45.582Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-24T14:31:09.994Z
+Commit: Fix broken links in documentation
