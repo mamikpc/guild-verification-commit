724c5201d3f8689d3104e5bde024e369eadf2a76
mamikpc <90100443+mamikpc@users.noreply.github.com>
2026-04-24 17:31:38 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T14:31:08.632Z
-Commit: Implement user profile customization
+Timestamp: 2026-04-24T14:31:38.078Z
+Commit: Fix broken links in documentation
