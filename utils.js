31a2704ef9f24b0a8ca2bc6a04c2a2b384ed50e8
mamikpc <90100443+mamikpc@users.noreply.github.com>
2026-04-24 17:32:21 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T14:32:14.495Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-24T14:32:20.677Z
+Commit: Add export functionality for reports
