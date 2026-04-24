8fc6353abbd9b7e0982af544c50dde1cbc01cca0
mamikpc <90100443+mamikpc@users.noreply.github.com>
2026-04-24 17:31:24 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T14:31:17.099Z
-Commit: Add multi-language support
+Timestamp: 2026-04-24T14:31:23.965Z
+Commit: Resolve null pointer exception in user service
